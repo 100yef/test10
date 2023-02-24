9eb1961fb9dee75eb6eed1200bebbfd82792eec3
Dmitrij <100yef@gmail.com>
2023-02-24 18:47:13 +0300

    classes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
-public class Main {
-    public static void main(String[] args) {
+import app.Application;
+import io.github.humbleui.jwm.App;
 
+public class Main {
+
+    public static void main(String[] args) {
+        App.start(Application::new);
     }
+
 }
